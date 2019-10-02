fixed gradient modification, resnet added for n-bits

models/lenet.py
@@ -8,10 +8,10 @@ FLAGS = tf.app.flags.FLAGS
 
 class Lenet5(nn.Module):
 
-    def __init__(self):
+    def __init__(self, is_quantized):
         super(Lenet5, self).__init__()
 
-        self.is_quantized = FLAGS.is_quantized
+        self.is_quantized = is_quantized
         noise = FLAGS.noise_model
         n_bits_wt = FLAGS.n_bits_wt
         n_bits_act = FLAGS.n_bits_act

models/resnet.py
@@ -159,11 +159,11 @@ class ResNet_imagenet(ResNet):
         }
 
 
-class ResNet_cifar10(ResNet):
+class ResNet_cifar10_full_precision(ResNet):
 
     def __init__(self, num_classes=10,
                  block=BasicBlock, depth=18):
-        super(ResNet_cifar10, self).__init__()
+        super(ResNet_cifar10_full_precision, self).__init__()
         self.inplanes = 16
         n = int((depth - 2) / 6)
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1,

models/resnet_quantized.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 import torchvision.transforms as transforms
 import math
-from ..quantize import QConv2d, QLinear
+from quantize import QConv2d, QLinear
 import tensorflow as tf
 
 __all__ = ['resnet_quantized_float_bn']
@@ -113,7 +113,7 @@ class ResNet(nn.Module):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
-                QConv2d(in_channels=self.inplanes, out_channels=planes * block.expansion, kernel_size=(1,),
+                QConv2d(in_channels=self.inplanes, out_channels=planes * block.expansion, kernel_size=(1,1),
                         stride=stride, bias=False, is_quantized=self.is_quantized, num_bits_weight=n_bits_wt,
                         num_bits_act=n_bits_act),
 
@@ -122,13 +122,15 @@ class ResNet(nn.Module):
 
         layers = []
         layers.append(block(self.inplanes, planes, stride, downsample))
+
         self.inplanes = planes * block.expansion
+
         for i in range(1, blocks):
             layers.append(block(self.inplanes, planes))
 
         return nn.Sequential(*layers)
 
-    def forward(self, x, eta):
+    def forward(self, x, eta=0.):
         x = self.conv1(x, eta)
         x = self.bn1(x)
         x = self.relu(x)
@@ -181,21 +183,34 @@ class ResNet_cifar10(ResNet):
     def __init__(self, num_classes=10,
                  block=BasicBlock, depth=18):
         super(ResNet_cifar10, self).__init__()
-        self.inplanes = 16
+
+        if n_bits_wt<=2:
+            self.inflate = 5
+
+        elif n_bits_wt <= 4:
+            self.inflate = 2
+        else:
+            self.inflate = 1
+
+        self.is_quantized = FLAGS.is_quantized
+        self.noise = FLAGS.noise_model
+
+        self.inplanes = 16 * self.inflate
+
         n = int((depth - 2) / 6)
-        self.conv1 = QConv2d(in_channels=3, out_channels=16, kernel_size=(3,3), stride=1, padding=1,
+        self.conv1 = QConv2d(in_channels=3, out_channels=16*self.inflate, kernel_size=(3,3), stride=1, padding=1,
                              bias=False, is_quantized=self.is_quantized, num_bits_weight=n_bits_wt, num_bits_act=n_bits_act)
 
-        self.bn1 = nn.BatchNorm2d(16)
+        self.bn1 = nn.BatchNorm2d(16 * self.inflate)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = lambda x: x
-        self.layer1 = self._make_layer(block, 16, n)
-        self.layer2 = self._make_layer(block, 32, n, stride=2)
-        self.layer3 = self._make_layer(block, 64, n, stride=2)
+        self.layer1 = self._make_layer(block, 16 * self.inflate, n)
+        self.layer2 = self._make_layer(block, 32 * self.inflate, n, stride=2)
+        self.layer3 = self._make_layer(block, 64 * self.inflate, n, stride=2)
         self.layer4 = lambda x: x
         self.avgpool = nn.AvgPool2d(8)
-        self.fc = QLinear(64, num_classes, num_bits=NUM_BITS, num_bits_weight=NUM_BITS_WEIGHT,
-                          num_bits_grad=NUM_BITS_GRAD)
+        self.fc = QLinear(64 * self.inflate, num_classes, is_quantized=self.is_quantized, noise=self.noise,
+                           num_bits_weight=n_bits_wt, num_bits_act=n_bits_act)
 
         init_model(self)
         self.regime = [
@@ -208,29 +223,33 @@ class ResNet_cifar10(ResNet):
 
 
 def resnet_quantized_float_bn(**kwargs):
-    num_classes, depth, dataset = map(
-        kwargs.get, ['num_classes', 'depth', 'dataset'])
-    if dataset == 'imagenet':
-        num_classes = num_classes or 1000
-        depth = depth or 50
-        if depth == 18:
-            return ResNet_imagenet(num_classes=num_classes,
-                                   block=BasicBlock, layers=[2, 2, 2, 2])
-        if depth == 34:
-            return ResNet_imagenet(num_classes=num_classes,
-                                   block=BasicBlock, layers=[3, 4, 6, 3])
-        if depth == 50:
-            return ResNet_imagenet(num_classes=num_classes,
-                                   block=Bottleneck, layers=[3, 4, 6, 3])
-        if depth == 101:
-            return ResNet_imagenet(num_classes=num_classes,
-                                   block=Bottleneck, layers=[3, 4, 23, 3])
-        if depth == 152:
-            return ResNet_imagenet(num_classes=num_classes,
-                                   block=Bottleneck, layers=[3, 8, 36, 3])
+    #TODO:fix for imagenet (and resnet34)
 
-    elif dataset == 'cifar10':
-        num_classes = num_classes or 10
-        depth = depth or 56
-        return ResNet_cifar10(num_classes=num_classes,
-                              block=BasicBlock, depth=depth)
+    # num_classes, depth, dataset = map(
+    #     kwargs.get, ['num_classes', 'depth', 'dataset'])
+    # if dataset == 'imagenet':
+    #     num_classes = num_classes or 1000
+    #     depth = depth or 50
+    #     if depth == 18:
+    #         return ResNet_imagenet(num_classes=num_classes,
+    #                                block=BasicBlock, layers=[2, 2, 2, 2])
+    #     if depth == 34:
+    #         return ResNet_imagenet(num_classes=num_classes,
+    #                                block=BasicBlock, layers=[3, 4, 6, 3])
+    #     if depth == 50:
+    #         return ResNet_imagenet(num_classes=num_classes,
+    #                                block=Bottleneck, layers=[3, 4, 6, 3])
+    #     if depth == 101:
+    #         return ResNet_imagenet(num_classes=num_classes,
+    #                                block=Bottleneck, layers=[3, 4, 23, 3])
+    #     if depth == 152:
+    #         return ResNet_imagenet(num_classes=num_classes,
+    #                                block=Bottleneck, layers=[3, 8, 36, 3])
+    #
+    # elif dataset == 'cifar10':
+    #     num_classes = num_classes or 10
+    #     depth = depth or 56
+    #     return ResNet_cifar10(num_classes=num_classes,
+    #                           block=BasicBlock, depth=depth)
+
+    return ResNet_cifar10(num_classes=10, block=BasicBlock)

quantize.py
@@ -22,13 +22,17 @@ def _mean(p, dim):
         return _mean(p.transpose(0, dim), 0).transpose(0, dim)
 
 
-# class UniformQuantize(InplaceFunction):
-class UniformQuantize(torch.autograd.Function):
+class UniformQuantize(InplaceFunction):
+# class UniformQuantize(torch.autograd.Function):
 # class UniformQuantize(nn.Module):
 
-    @classmethod
-    def forward(cls, ctx, input, num_bits=8, min_value=None, max_value=None, eta = .01,
-                noise=None, num_chunks=None, out_half=False):
+    # @classmethod
+
+    @staticmethod
+    # def forward(cls, ctx, input, num_bits=8, min_value=None, max_value=None, eta = .01,
+    #             noise=None, num_chunks=None, out_half=False):
+    def forward(ctx, input, num_bits=8, min_value=None, max_value=None, eta=.01,
+                    noise=None, num_chunks=None, out_half=False):
 
         num_chunks = input.shape[0] if num_chunks is None else num_chunks
         if min_value is None or max_value is None:
@@ -46,13 +50,13 @@ class UniformQuantize(torch.autograd.Function):
 ##
         ctx.noise = noise
 
-
         if quantize:
 
             ctx.num_bits = num_bits
             ctx.min_value = min_value
             ctx.max_value = max_value
-
+            # print(input)
+            # exit()
             q_weight = input.clone()
 
             qmin = 0.
@@ -113,15 +117,23 @@ class UniformQuantize(torch.autograd.Function):
     def backward(ctx, grad_output):
         # straight-through estimator
 
-        if FLAGS.regularization=='l2':
-            pert = ctx.saved_tensors[0]
-            grad_input = grad_output + FLAGS.gamma * FLAGS.diag_load_const * 2 * pert
-        elif FLAGS.regularization=='fisher':
-            pert = ctx.saved_tensors[0]
-            #TODO: incorporate diagonal load amount in a flag
-            grad_input = grad_output + FLAGS.gamma * 2 * (grad_output * grad_output * pert + FLAGS.diag_load_const * pert)
-        else:
-            grad_input = grad_output
+        # if FLAGS.regularization=='l2':
+        #     pert = ctx.saved_tensors[0]
+        #     grad_input = grad_output + FLAGS.gamma * FLAGS.diag_load_const * 2 * pert
+        # elif FLAGS.regularization=='fisher':
+        #     pert = ctx.saved_tensors[0]
+        #
+        #     # grad_input = grad_output + torch.clamp(FLAGS.gamma *
+        #     #                                        2 * (grad_output * grad_output *
+        #     #                                        pert + FLAGS.diag_load_const * pert),
+        #     #                                        -.1, .1)
+        #
+        #     grad_input = grad_output + FLAGS.gamma * 2 * (grad_output * grad_output * pert + FLAGS.diag_load_const * pert)
+        #
+        #
+        # else:
+        #     grad_input = grad_output
+        grad_input = grad_output
 
         # return grad_input
         return grad_input, None, None, None, None, None, None, None, None
@@ -211,7 +223,7 @@ class QuantMeasure(nn.Module):
 
     def forward(self, input):
 
-        if self.training:
+        if self.training and FLAGS.regularization is None:
             # std = torch.std(input.detach().view(-1))
             # mean = torch.mean(input.detach().view(-1))
 
@@ -223,12 +235,12 @@ class QuantMeasure(nn.Module):
             # ind_min = int(n_elems * .001)
             # ind_max = n_elems - ind_min
 
-            if self.num_bits<=2:
+            # if self.num_bits<=2:
 
-                min_value = -.5
-                max_value = .5
+                # min_value = -.5
+                # max_value = .5
 
-            else:
+            # else:
 
                 # std = torch.std(input.detach().view(-1))
                 # mean = torch.mean(input.detach()
@@ -237,8 +249,8 @@ class QuantMeasure(nn.Module):
                 # min_value = mean - 3 * std
                 # max_value = mean + 3 * std
 
-                min_value = input.detach().view(input.size(0), -1).min(-1)[0].mean()
-                max_value = input.detach().view(input.size(0), -1).max(-1)[0].mean()
+            min_value = input.detach().view(input.size(0), -1).min(-1)[0].mean()
+            max_value = input.detach().view(input.size(0), -1).max(-1)[0].mean()
 
                 # min_value = torch.kthvalue(input.view(-1), ind_min)[0]
                 # max_value = torch.kthvalue(input.view(-1), ind_max)[0]
@@ -266,9 +278,8 @@ class QConv2d(nn.Conv2d):
         self.is_quantized = is_quantized
 
 
-        if self.num_bits_act<32:
+        if self.num_bits_act<32 and is_quantized:
             self.quantize_input = QuantMeasure(self.num_bits_act)
-
 
         self.biprecision = biprecision
         self.noise = noise
@@ -282,7 +293,7 @@ class QConv2d(nn.Conv2d):
     #TODO: add inputs eta, noise model (eta kept in formward
 
 
-    def forward(self, input, eta):
+    def forward(self, input, eta=0.):
 
         #Eta is kept as an input to the forward() function since eta_train=\=eta_inf sometimes
 
@@ -297,7 +308,7 @@ class QConv2d(nn.Conv2d):
 
             if FLAGS.q_min is None:
                 self.min_value = self.weight.min()
-                print(self.min_value)
+                # print(self.min_value)
 
             if FLAGS.q_max is None:
                 self.max_value = self.weight.max()
@@ -306,25 +317,24 @@ class QConv2d(nn.Conv2d):
             # self.weight.data.clamp(self.min_value.detach().cpu().numpy(), self.max_value.detach().cpu().numpy())
             # self.weight.data = tensor_clamp(self.weight, self.min_value, self.max_value)
 
-            qweight = quantize(self.weight, num_bits=self.num_bits_weight,
+            self.qweight = quantize(self.weight, num_bits=self.num_bits_weight,
                                min_value=float(self.min_value),
                                max_value=float(self.max_value), noise=self.noise, eta=eta)
 
 
-            self.qweight = qweight.clone()
 
             #TODO: ADD NOISING FUNCTION HERE
 
 
             if self.bias is not None:
-                qbias = quantize(self.bias, num_bits=self.num_bits_weight)
+                self.qbias = quantize(self.bias, num_bits=self.num_bits_weight)
 
             else:
-                qbias = None
+                self.qbias = None
 
 
 
-            output = F.conv2d(qinput, qweight, qbias, self.stride,
+            output = F.conv2d(qinput, self.qweight, self.qbias, self.stride,
                                   self.padding, self.dilation, self.groups)
 
         else:
@@ -345,7 +355,7 @@ class QLinear(nn.Linear):
         self.num_bits_weight = num_bits_weight
         self.is_quantized = is_quantized
 
-        if self.num_bits_act<32:
+        if self.num_bits_act<32 and is_quantized:
             self.quantize_input = QuantMeasure(self.num_bits_act)
 
         self.noise = noise
@@ -356,7 +366,7 @@ class QLinear(nn.Linear):
         if FLAGS.q_max is not None:
             self.max_value = torch.tensor(FLAGS.q_max, device='cuda')
 
-    def forward(self, input, eta):
+    def forward(self, input, eta=0.):
 
         if self.is_quantized:
             if self.num_bits_act < 32:
@@ -373,18 +383,18 @@ class QLinear(nn.Linear):
             # self.weight.data.clamp(self.min_value, self.max_value)
             # self.weight.data = tensor_clamp(self.weight, self.min_value, self.max_value)
 
-            qweight = quantize(self.weight, num_bits=self.num_bits_weight,
+            self.qweight = quantize(self.weight, num_bits=self.num_bits_weight,
                                min_value=float(self.min_value),
                                max_value=float(self.max_value), noise=self.noise, eta=eta)
 
             #TODO: choose whether to quantize bias
             if self.bias is not None:
-                qbias = quantize(self.bias, num_bits=self.num_bits_weight)
+                self.qbias = quantize(self.bias, num_bits=self.num_bits_weight)
             else:
-                qbias = None
+                self.qbias = None
 
 
-            output = F.linear(qinput, qweight, qbias)
+            output = F.linear(qinput, self.qweight, self.qbias)
 
         else:
 

train_resnet18.py
@@ -2,7 +2,6 @@ from data import get_dataset
 import torch.nn as nn
 import torch.backends.cudnn as cudnn
 from preprocess import get_transform
-import models
 import matplotlib.pyplot as plt
 from utils.model_utils import *
 from utils.dataset_utils import *
@@ -14,15 +13,26 @@ import time as time
 import numpy as np
 import os
 
-tf.app.flags.DEFINE_string( 'dataset', 'fashionmnist', 'either mnist or fashionmnist')
+'''
+
+n epochs = 300
+cosine decay rate
+weight decay
+
+'''
+
+tf.app.flags.DEFINE_string( 'dataset', 'cifar10', 'either mnist or fashionmnist')
 tf.app.flags.DEFINE_integer( 'batch_size', 128, 'batch size')
-tf.app.flags.DEFINE_integer('n_epochs', 25, 'num epochs' )
+tf.app.flags.DEFINE_integer('n_epochs', 100, 'num epochs' )
 tf.app.flags.DEFINE_integer('record_interval', 100, 'how many iterations between printing to console')
+tf.app.flags.DEFINE_float('weight_decay', 1e-4, 'weight decay value')
+
 tf.app.flags.DEFINE_float('lr', 1e-3, 'learning rate')
 
-tf.app.flags.DEFINE_boolean('is_quantized', True, 'whether the network is quantized')
-tf.app.flags.DEFINE_integer('n_bits_act', 8, 'number of bits activation')
-tf.app.flags.DEFINE_integer('n_bits_wt', 8, 'number of bits weight')
+# tf.app.flags.DEFINE_boolean('is_quantized', True, 'whether the network is quantized')
+tf.app.flags.DEFINE_boolean('is_quantized', False, 'whether the network is quantized')
+tf.app.flags.DEFINE_integer('n_bits_act', 32, 'number of bits activation')
+tf.app.flags.DEFINE_integer('n_bits_wt', 32, 'number of bits weight')
 tf.app.flags.DEFINE_float('eta', .0, 'noise eta')
 
 tf.app.flags.DEFINE_string('noise_model', None, 'type of noise to add None, NVM, or PCM')
@@ -45,11 +55,12 @@ tf.app.flags.DEFINE_string('loadpath', None, 'directory to load model from')
 tf.app.flags.DEFINE_boolean('debug', False, 'if debug mode or not, in debug mode, model is not saved')
 
 #Distillation Params
-tf.app.flags.DEFINE_string('fp_loadpath', './SavedModels/Lenet/FP/Run0/lenet', 'path to FP model for loading for distillation')
+tf.app.flags.DEFINE_string('fp_loadpath', './SavedModels/Resnet18/FP/Run0/resnet', 'path to FP model for loading for distillation')
 tf.app.flags.DEFINE_float('alpha', 1.0, 'distillation regularizer multiplier')
 tf.app.flags.DEFINE_float('temperature', 1.0, 'temperature for distillation')
 
 
+from models.resnet_quantized import ResNet_cifar10
 
 FLAGS = tf.app.flags.FLAGS
 
@@ -116,7 +127,7 @@ if FLAGS.noise_model is not None:
 #Save path
 if not FLAGS.debug and FLAGS.savepath is None:
 
-    SAVEPATH = './SavedModels/Lenet/%dba_%dbw' % (n_bits_act, n_bits_wt)
+    SAVEPATH = './SavedModels/Resnet18/%dba_%dbw' % (n_bits_act, n_bits_wt)
     if reg_string is not '':
         SAVEPATH += '_' + reg_string
     SAVEPATH += '/'
@@ -161,7 +172,8 @@ test_accs=[]
 
 for k in range(n_runs):
     i=0
-    model = models.Lenet5(is_quantized=FLAGS.is_quantized)
+    model = ResNet_cifar10()
+    # exit()
     model.cuda()
     optimizer = optim.Adam(model.parameters(), lr=FLAGS.lr)
     # print(model.conv1.quantize_input)
@@ -180,7 +192,7 @@ for k in range(n_runs):
     if distillation:
         checkpoint = torch.load(FLAGS.fp_loadpath)
 
-        teacher_model = models.Lenet5(is_quantized=False)
+        teacher_model = ResNet_cifar10()
         teacher_model.cuda()
         teacher_model.load_state_dict(checkpoint['model_state_dict'])
 
@@ -203,7 +215,7 @@ for k in range(n_runs):
             inputs = inputs.cuda()
             targets = targets.cuda()
 
-            output = model(inputs, eta=etaval)
+            output = model(inputs)
             loss = criterion(output, targets)
 
             #adding distillation loss